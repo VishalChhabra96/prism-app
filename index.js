3b1256085005119957cad91b5af60b2fb3d26510
Vishal Chhabra <chhabra.vishal96@gmail.com>
2020-01-15 16:55:33 +0530

    code refactoring

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     $('input[type="radio"]').click(function(){
         var inputValue = $(this).attr("value");
@@ -6,34 +5,38 @@ $(document).ready(function(){
         $(".box").not(targetBox).hide();
         $(targetBox).show();
     });
+    $("#linkid1").click();
 });
+
+function toggleView(page) {
+    if(page === 'PrismSetup') {
+        $("#linkid1").click();
+    }
+    if(page === 'ExecutionPage') {
+        $("#linkid2").click();
+    }
+}
 
 function save2File(type) {
     let data = '';
+    let name = document.getElementById(`${type}-usr`);
+    let sel0 = document.getElementById(`${type}-sel0`);
+    let sel1 = document.getElementById(`${type}-sel1`);
     if(type === 'api') {
-        let name = document.getElementById(`${type}-usr`);
-        let sel0 = document.getElementById(`${type}-sel0`);
-        let sel1 = document.getElementById(`${type}-sel1`);
         data = name.value + '\n' + sel0.value + '\n' + sel1.value;
     }
     else if(type === 'mobile') {
-        let name = document.getElementById(`${type}-usr`);
-        let sel0 = document.getElementById(`${type}-sel0`);
-        let sel1 = document.getElementById(`${type}-sel1`);
         let sel2 = document.getElementById(`${type}-sel2`);
         data = name.value + '\n' + sel0.value + '\n' + sel1.value + '\n' + sel2.value;
     }
     else
     {
-        let name = document.getElementById(`${type}-usr`);
-        let sel0 = document.getElementById(`${type}-sel0`);
-        let sel1 = document.getElementById(`${type}-sel1`);
         let sel2 = document.getElementById(`${type}-sel2`);
         let sel3 = document.getElementById(`${type}-sel3`);
         data = name.value + '\n' + sel0.value + '\n' + sel1.value + '\n' + sel2.value + '\n' + sel3.value;
     }
     let textToBLOB = new Blob([data], { type: 'text/plain' });
-    let sFileName = `${type}FormInputs.txt`;
+    let sFileName = `${type}-FormInputs.txt`;
     let newLink = document.createElement("a");
     newLink.download = sFileName;
     if (window.webkitURL != null) {
